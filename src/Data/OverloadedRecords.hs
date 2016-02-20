e44cf74784824aca0b7701f6c79eff3bba9f9ca2
Peter Trško <peter.trsko@gmail.com>
2016-02-20 13:51:16 +0100

    Renamed type class UpdateField to SetField

--- src/Data/OverloadedRecords.hs
+++ src/Data/OverloadedRecords.hs
@@ -18,7 +18,7 @@ module Data.OverloadedRecords
     , HasField(..)
 
     , UpdateType
-    , UpdateField(..)
+    , SetField(..)
     )
   where
 
@@ -35,15 +35,16 @@ type family UpdateType (l :: Symbol) (s :: *) (a :: *) :: *
 
 -- Definition of this class is based on: https://phabricator.haskell.org/D1687
 class HasField (l :: Symbol) s a | l s -> a where
+    -- | Get value of a field.
     getField :: Proxy# l -> s -> a
 
 class
     ( HasField l s a
     , FieldType l s ~ a
     , UpdateType l s a ~ s
-    ) => UpdateField l s a
+    ) => SetField l s a
   where
-    updateField :: Proxy# l -> s -> a -> UpdateType l s a
+    setField :: Proxy# l -> s -> a -> UpdateType l s a
 
 type family FromArrow (a :: *) :: Bool where
     FromArrow (x -> y) = 'True
@@ -62,12 +63,12 @@ instance IsFieldFunction l x y (FromArrow x) => IsLabel l (x -> y) where
 instance
     ( Functor f
     , HasField l s a
-    , UpdateField l s b
+    , SetField l s b
     , FieldType l s ~ a
     , UpdateType l s b ~ t
     ) => IsFieldFunction l (a -> f b) (s -> f t) 'True
   where
-    fieldFunction proxy f s = updateField proxy s <$> f (getField proxy s)
+    fieldFunction proxy f s = setField proxy s <$> f (getField proxy s)
 
 -- | @'Proxy#' l -> r -> a@
 instance
