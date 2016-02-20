14f19a7de1b24cf6aa88b06baccf2593e48edeed
Peter Trško <peter.trsko@gmail.com>
2016-02-20 21:20:57 +0100

    Label related TH code in a separate module

---
+++ src/Data/OverloadedLabels/TH.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Data.OverloadedLabels.TH
+  where
+
+import Control.Applicative (Applicative((<*>)))
+import Data.Foldable (concat)
+import Data.Function ((.), ($))
+import Data.Functor (Functor(fmap), (<$>))
+import Data.String (String)
+import Data.Traversable (mapM, sequenceA)
+import GHC.Exts (Proxy#, proxy#)
+
+import Language.Haskell.TH
+    ( DecsQ
+    , litT
+    , mkName
+    , normalB
+    , sigD
+    , strTyLit
+    , valD
+    , varP
+    )
+
+import Data.OverloadedLabels
+
+
+label :: String -> DecsQ
+label l = (:) <$> sig <*> labelNoSig l
+  where
+    sig = mkName l `sigD` [t|forall a. IsLabel => $(litT $ strTyLit l) a => a|]
+
+labelNoSig :: String -> DecsQ
+labelNoSig l = sequenceA [varP (mkName l) `valD` normalB body $ []]
+  where
+    body = [| fromLabel (proxy# :: Proxy# $(litT $ strTyLit l)) |]
+
+labels :: [String] -> DecsQ
+labels = fmap concat . mapM label
+
+labelsNoSig :: [String] -> DecsQ
+labelsNoSig = fmap concat . mapM labelNoSig
